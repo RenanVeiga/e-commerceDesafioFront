5a2e7eaf16c5ec9fc6ff086f4d6764fa49775be5
Renan Morais da Veiga <renanmoraisveiga@gmail.com>
2022-04-05 12:09:26 -0300

    projetoSiteDesafioFront

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    CarrinhoComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
